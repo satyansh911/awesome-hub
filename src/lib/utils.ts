93b28e182f7c958e8519691a039503cfff22a685
nafisreza <bigganinafis@gmail.com>
2025-10-03 21:48:30 +0600

    feat: revamp ui and integrate shadcn/ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,39 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function formatNumber(num: number): string {
-  if (num >= 1000000) {
-    return (num / 1000000).toFixed(1) + 'M'
-  } else if (num >= 1000) {
-    return (num / 1000).toFixed(1) + 'K'
-  }
-  return num.toString()
-}
-
-export function formatDate(dateString: string): string {
-  const date = new Date(dateString)
-  return date.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric'
-  })
-}
-
-export function extractAwesomeCategory(repoName: string): string {
-  const name = repoName.toLowerCase()
-  if (name.startsWith('awesome-')) {
-    return name.replace('awesome-', '').replace(/-/g, ' ')
-  }
-  return 'general'
-}
-
-export function generateSlug(text: string): string {
-  return text
-    .toLowerCase()
-    .replace(/[^\w ]+/g, '')
-    .replace(/ +/g, '-')
 }
